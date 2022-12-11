18164d35b94a774f357a7bb5c68fa41a621dbc7e
Sayed-Ahammad-Sani <sayedsani023@gmail.com>
2022-12-11 10:18:13 +0600

    First commit with jetstream auth

---
+++ resources/views/layouts/back_end/app.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <script src="https://kit.fontawesome.com/ba2b6acfb0.js" crossorigin="anonymous"></script>
+    <style>
+        .app-container {
+            display: none;
+        }
+    </style>
+    <!-- Styles -->
+    @livewireStyles
+</head>
+
+<body class="font-sans antialiased">
+    {{-- <div id="pre-loader">
+        <img src="{{asset('img/pre-loader.gif')}}" />
+    </div> --}}
+    <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+
+        {{-- header --}}
+        @include('layouts.back_end.partials.header')
+
+        <div class="app-main">
+
+            {{-- sidebar --}}
+            @include('layouts.back_end.partials.side_bar')
+
+            <div class="app-main__outer">
+                <div class="app-main__inner">
+                    @yield('dashboard-inner-content')
+                </div>
+
+                {{-- footer --}}
+                @include('layouts.back_end.partials.footer')
+
+            </div>
+            <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        </div>
+
+        <!-- Page Content -->
+        {{-- <main>
+            {{ $slot }}
+        </main> --}}
+    </div>
+
+    @stack('modals')
+    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{asset('js/main.js')}}"></script>
+    <script src="{{asset('js/demo.js')}}"></script>
+    <script src="{{asset('js/chart_js.js')}}"></script>
+    <style>
+        .app-header__logo .logo-src {
+            height: 23px;
+            width: 97px;
+            background: url(https://ahammadit.com/wp-content/uploads/2022/11/ahammad-it-logo.png);
+            background-size: contain;
+            background-repeat: no-repeat;
+        }
+    </style>
+    @livewireScripts
+</body>
+
+</html>
---
+++ resources/views/layouts/back_end/partials/side_bar.blade.php
@@ -0,0 +1,204 @@
+<div class="app-sidebar sidebar-shadow">
+    <div class="app-header__logo">
+        <div class="logo-src"></div>
+        <div class="header__pane ms-auto">
+            <div>
+                <button type="button" class="hamburger close-sidebar-btn hamburger--elastic"
+                    data-class="closed-sidebar">
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="app-header__mobile-menu">
+        <div>
+            <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
+            </button>
+        </div>
+    </div>
+    <div class="app-header__menu">
+        <span>
+            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                <span class="btn-icon-wrapper">
+                    <i class="fa fa-ellipsis-v fa-w-6"></i>
+                </span>
+            </button>
+        </span>
+    </div>
+    <div class="scrollbar-sidebar">
+        <div class="app-sidebar__inner">
+            <ul class="vertical-nav-menu">
+                <li class="app-sidebar__heading">Dashboards</li>
+                <li>
+                    <a href="index.html" class="mm-active">
+                        <i class="fa-solid fa-rocket metismenu-icon"></i>
+                        Dashboard Example 1
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">UI Components</li>
+                <li>
+                    <a href="#">
+                        <i class="fa-regular fa-gem metismenu-icon"></i>
+                        Elements
+                        <i class="fa-solid fa-angle-down metismenu-state-icon caret-left"></i>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="elements-buttons-standard.html">
+                                <i class="metismenu-icon"></i>
+                                Buttons
+                            </a>
+                        </li>
+                        <li>
+                            <a href="elements-dropdowns.html">
+                                <i class="metismenu-icon"> </i>Dropdowns
+                            </a>
+                        </li>
+                        <li>
+                            <a href="elements-icons.html">
+                                <i class="metismenu-icon"> </i>Icons
+                            </a>
+                        </li>
+                        <li>
+                            <a href="elements-badges-labels.html">
+                                <i class="metismenu-icon"> </i>Badges
+                            </a>
+                        </li>
+                        <li>
+                            <a href="elements-cards.html">
+                                <i class="metismenu-icon"> </i>Cards
+                            </a>
+                        </li>
+                        <li>
+                            <a href="elements-list-group.html">
+                                <i class="metismenu-icon"> </i>List Groups
+                            </a>
+                        </li>
+                        <li>
+                            <a href="elements-navigation.html">
+                                <i class="metismenu-icon"> </i>Navigation Menus
+                            </a>
+                        </li>
+                        <li>
+                            <a href="elements-utilities.html">
+                                <i class="metismenu-icon"> </i>Utilities
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa-solid fa-car metismenu-icon"></i>
+                        Components
+                        <i class="fa-solid fa-angle-down metismenu-state-icon caret-left"></i>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="components-tabs.html">
+                                <i class="metismenu-icon"> </i>Tabs
+                            </a>
+                        </li>
+                        <li>
+                            <a href="components-accordions.html">
+                                <i class="metismenu-icon"> </i>Accordions
+                            </a>
+                        </li>
+                        <li>
+                            <a href="components-notifications.html">
+                                <i class="metismenu-icon"> </i>Notifications
+                            </a>
+                        </li>
+                        <li>
+                            <a href="components-modals.html">
+                                <i class="metismenu-icon"> </i>Modals
+                            </a>
+                        </li>
+                        <li>
+                            <a href="components-progress-bar.html">
+                                <i class="metismenu-icon"> </i>Progress Bar
+                            </a>
+                        </li>
+                        <li>
+                            <a href="components-tooltips-popovers.html">
+                                <i class="metismenu-icon"> </i>Tooltips &amp; Popovers
+                            </a>
+                        </li>
+                        <li>
+                            <a href="components-carousel.html">
+                                <i class="metismenu-icon"> </i>Carousel
+                            </a>
+                        </li>
+                        <li>
+                            <a href="components-calendar.html">
+                                <i class="metismenu-icon"> </i>Calendar
+                            </a>
+                        </li>
+                        <li>
+                            <a href="components-pagination.html">
+                                <i class="metismenu-icon"> </i>Pagination
+                            </a>
+                        </li>
+                        <li>
+                            <a href="components-scrollable-elements.html">
+                                <i class="metismenu-icon"> </i>Scrollable
+                            </a>
+                        </li>
+                        <li>
+                            <a href="components-maps.html">
+                                <i class="metismenu-icon"> </i>Maps
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="tables-regular.html">
+                        <i class="fa-solid fa-table-list metismenu-icon"></i>
+                        {{-- <i class=""></i> --}}
+                        Tables
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">Widgets</li>
+                <li>
+                    <a href="dashboard-boxes.html">
+                        <i class="fa-solid fa-box-open metismenu-icon"></i>
+                        Dashboard Boxes
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">Forms</li>
+                <li>
+                    <a href="forms-controls.html">
+                        <i class="metismenu-icon fa-solid fa-computer-mouse"> </i>
+                        <i class=""></i>
+                        Forms Controls
+                    </a>
+                </li>
+                <li>
+                    <a href="forms-layouts.html">
+                        <i class="metismenu-icon fa-solid fa-eye-dropper"> </i>
+                        Forms
+                        Layouts
+                    </a>
+                </li>
+                <li>
+                    <a href="forms-validation.html">
+                        <i class="metismenu-icon fa-solid fa-check"> </i>
+                        Forms
+                        Validation
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">Charts</li>
+                <li>
+                    <a href="charts-chartjs.html">
+                        <i class="metismenu-icon fa-solid fa-chart-line"></i>
+                        ChartJS
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
